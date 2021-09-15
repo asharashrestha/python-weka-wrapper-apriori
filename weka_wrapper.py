f48ca17d66612279ceaf339338b8d1a1a1d1a4c5
Ashara <ashara.shrestha@gmail.com>
2021-09-15 00:04:44 -0500

    Confidence progression Order Detection and corresponding order of events implemented

--- weka_wrapper.py
+++ weka_wrapper.py
@@ -12,6 +12,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Copyright (C) 2014-2018 Fracpete (pythonwekawrapper at gmail dot com)
+from os import replace
 import pandas as pd
 import sys
 import traceback
@@ -22,8 +23,25 @@ from weka.associations import Associator
 import javabridge
 from javabridge import JWrapper
 import re
-import itertools
+from itertools import permutations
 from treelib import Node, Tree
+
+
+def is_order(lst):
+    if lst == sorted(lst, reverse=False):
+        return 'Ascending'
+    elif lst == sorted(lst, reverse=True):
+        return 'Descending'
+    else:
+        return 'Neither ascending nor descending'
+
+
+def perm_elems(lst):
+    p_list = []
+    perm_list = permutations(lst, len(lst))
+    for i in perm_list:
+        p_list.append(', '.join(i))
+    return p_list
 
 
 # create Tree structure for blocks of rules to show rule progression.
@@ -34,19 +52,18 @@ def createRulesTree(df):
         if ',' not in lhs:  # keeping only those rules where # of elem in lhs and rhs is 1.
             tree = Tree()
             rules_queue = []
-            conf_queue = []
             tree.create_node(lhs + "->" + rhs + "[Conf: " + row["Conf"] + "]", "root")  # root node
-            conf_queue.append((lhs + "->" + rhs, row["Conf"]))
+            l_list = lhs.split(",")
+            l_list.append(rhs)
+            a = perm_elems(l_list)
             for index, row in df.iterrows():
-                left_rule = lhs + ', ' + rhs
-                if (row['LHS'] == left_rule):
+
+                if any(row['LHS'] == elem for elem in a):
                     left = row['LHS']
                     right = row['RHS']
                     a = left + ", " + right
-                    # print("Id is: ", a)
                     tree.create_node(left + "->" + right + "[Conf:" + row["Conf"] + "]", a, parent="root")
                     rules_queue.append(left + ", " + right)
-                    conf_queue.append((a, row["Conf"]))
 
             while len(rules_queue) != 0:
                 elem = rules_queue.pop(0)
@@ -57,21 +74,34 @@ def createRulesTree(df):
                         a = left + ", " + right  # concatenating lhs and rhs to find the result in lhs of dataframe
                         tree.create_node(left + "->" + right + "[Conf:" + row["Conf"] + "]", a, parent=left)
                         rules_queue.append(left + ", " + right)
-                        conf_queue.append((a, row["Conf"]))
-            print("----------------TREE------------------------")
-            tree.show(line_type="ascii-em")
-            print("----------------TREE------------------------")
-            a = conf_queue[-1]
-            for node in conf_queue:
-                node = node[0]
-                if node.count(",") == 0:
-                    print(','.join([tree[node].tag for node in tree.rsearch("root")]))
+            if tree.depth() > 0:
+                print("==================================BLOCK==================================")
+                tree.show(line_type="ascii-em")
+
+                for path in tree.paths_to_leaves():
+                    print("-----------------Confidence Progression-----------------")
+                    rule_sequence = ""
+                    conf_sequence = ""
+                    for i in path:
+                        left_attr = ""
+                        a = tree.get_node(i).tag
+                        special_characters = '[[|]|(|)]'  # removing "[", "]" from confidence
+                        conf = re.sub(special_characters, '', a.split("Conf:")[1])
+                        conf = conf.strip().replace("(", "").replace(")", "")
+                        right = a.split("Conf:")[0].split("->")[1].split("=")[0]
+                        left = a.split("->")[0].split(",")
+                        left_attr = ", ".join(l.split("=")[0] for l in left)
+                        if rule_sequence == "":
+                            rule_sequence = left_attr + " -> " + right
+                            conf_sequence = conf
+                        else:
+                            rule_sequence += " -> " + right
+                            conf_sequence += " -> " + conf
+                    print("Rule Sequence: " + rule_sequence)
+                    print("Confidence Sequence: " + conf_sequence)
+                    conf_seq_list = [float(i) for i in conf_sequence.split("->")]
+                    print(is_order(conf_seq_list))
                     print("\n")
-                else:
-                    print(','.join([tree[node].tag for node in tree.rsearch(node)]))
-                    print("\n")
-            print("=======================================")
-            print("\n")
 
 
 def main(args):
@@ -80,11 +110,10 @@ def main(args):
     """
     # load a dataset
     if len(args) <= 1:
-        data_file = "/Users/ashara/Documents/Study/Research/Dissertation/One Drive/OneDrive - University of Texas at Arlington/Dissertation/data_files/preprocessed_4_attr.arff"
+        data_file = "/Users/ashara/Documents/Study/Research/Dissertation/One Drive/OneDrive - University of Texas at Arlington/Dissertation/data_files/processed_claims_3_cond_included_no_treatment_non-resp.arff"
     helper.print_info("Loading dataset: " + data_file)
     loader = Loader("weka.core.converters.ArffLoader")
     data = loader.load_file(data_file)
-    data.class_is_last()
 
     # build Apriori, using last attribute as class attribute
     apriori = Associator(classname="weka.associations.Apriori",
@@ -92,7 +121,7 @@ def main(args):
     apriori.build_associations(data)
 
     # iterate association rules (low-level)
-    helper.print_info("Rules list")
+    helper.print_info("****** Rules List ******")
     # make the underlying rules list object iterable in Python
     rules = javabridge.iterate_collection(apriori.jwrapper.getAssociationRules().getRules().o)
 
@@ -101,11 +130,13 @@ def main(args):
     p_conf = re.compile('<conf:(.*)>')
     for i, rule in enumerate(rules):
         # wrap the Java object to make its methods accessible
-        rule = str(JWrapper(rule))
+        rule = JWrapper(rule)
+        print(str(i + 1) + ". " + str(rule))
+        rule = str(rule)
         lhs = rule.split("==>")[0]
         lhs_count = lhs.split(": ")[1]
         rhs = rule.split("==> ")[1]
-        rhs_count = 0
+        rhs_count = 0  # LOGIC: Change this and apply logic
         conf = p_conf.findall(rule)[0]
         lhs = rule.split(":")[0].replace("[", "").replace("]", "")
         rhs = rhs.split("<conf")[0].split(":")[0].replace("[", "").replace("]", "")
