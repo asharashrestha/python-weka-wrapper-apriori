88771107bdda39440a46600e5a9e9d2768011c96
Ashara <ashara.shrestha@gmail.com>
2021-10-24 16:12:03 -0500

    Lift added also when number of attributes is 2

--- weka_wrapper.py
+++ weka_wrapper.py
@@ -76,14 +76,14 @@ def show_tree(num_of_attr):
 
 # create Tree structure for blocks of rules to show rule progression.
 def createRulesTree(num_attr, df, r_param):
-    # df.to_csv("Dataframe.csv")
+    df.to_csv("Dataframe.csv")
     
     for index, row in df.iterrows():
         if "," not in row['LHS']: # to start with root node having only two attributes
             lhs = row['LHS']
             rhs = row['RHS']
-            conf = row['Conf']
-            lift = row['Lift']
+            conf = str(round(float(row['Conf']),2))
+            lift = str(round(float(row['Lift']), 2))
 
             tree = Tree()
             rules_queue = []
@@ -95,8 +95,10 @@ def createRulesTree(num_attr, df, r_param):
                 if any(row['LHS'] == elem for elem in a):
                     left = row['LHS']
                     right = row['RHS']
+                    conf = str(round(float(row['Conf']),2))
+                    lift = str(round(float(row['Lift']), 2))
                     a = left + ", " + right
-                    tree.create_node(left + "->" + right + "[Conf:" + row["Conf"] + "]" + "[Lift:" + row["Lift"] + "]", a, parent="root")
+                    tree.create_node(left + "->" + right + "[Conf:" + conf + "]" + "[Lift:" + lift + "]", a, parent="root")
                     rules_queue.append(left + ", " + right)
 
             while len(rules_queue) != 0:
@@ -105,8 +107,10 @@ def createRulesTree(num_attr, df, r_param):
                     if (row['LHS'] == elem):
                         left = row['LHS']
                         right = row['RHS']
+                        conf = str(round(float(row['Conf']),2))
+                        lift = str(round(float(row['Lift']), 2))
                         a = left + ", " + right  # concatenating lhs and rhs to find the result in lhs of dataframe
-                        tree.create_node(left + "->" + right + "[Conf:" + row["Conf"] + "]"+ "[Lift:" + row["Lift"] + "]", a, parent=left)
+                        tree.create_node(left + "->" + right + "[Conf:" + conf + "]"+ "[Lift:" + lift + "]", a, parent=left)
                         rules_queue.append(left + ", " + right)
             # tree.show(line_type="ascii-em")
             # tree.save2file("Result_Tree.txt")
@@ -149,15 +153,6 @@ def createRulesTree(num_attr, df, r_param):
                 # r_param = "Mean Confidence"
                 # When parameter = Mean Confidence
                 if r_param == "Occurence":
-                    # if len(conf_seq_list) == 1:
-                    #     if rule_sequence in var_seq_order.keys():
-                    #         var_seq_order[rule_sequence]+=1
-                    #     else:
-                    #         var_seq_order[rule_sequence] = 1
-                    
-                    # if len(conf_seq_list) == 2:
-                    # high = float(conf_sequence.split("->")[-1])
-                    # low = float(conf_sequence.split("->")[0])
                     if rule_sequence in var_seq_order.keys():
                         var_seq_order[rule_sequence]+=1
                     else:
@@ -175,63 +170,71 @@ def createRulesTree(num_attr, df, r_param):
                             var_seq_order_lift[rule_sequence] = [lift_seq_list[0]]
                     high = float(conf_sequence.split("->")[-1])
                     low = float(conf_sequence.split("->")[0])
-                    # if (high - low >=0):
-                    if len(conf_seq_list) == 2:
-                        if rule_sequence in var_seq_order.keys():
-                            var_seq_order[rule_sequence].append(high - low)
-                        else:
-                            var_seq_order[rule_sequence] = [high-low]
 
-                    if len(conf_seq_list) > 2:
-                        if rule_sequence in var_seq_order.keys():
-                            var_seq_order[rule_sequence].append( high-low)
-                        else:
-                            var_seq_order[rule_sequence] = [high-low]
-
-
-                if r_param == "Ascendingness Consistency Ratio":
-                    if len(conf_seq_list) == 1:
-                        if rule_sequence in var_seq_order.keys():
-                            var_seq_order[rule_sequence].append(conf_seq_list[0])
-                        else:
-                            var_seq_order[rule_sequence] = [conf_seq_list[0]]
-                    high = float(conf_sequence.split("->")[-1])
-                    low = float(conf_sequence.split("->")[0])
-                    # if (high - low >=-5000):
-                    if len(conf_seq_list) == 2:
-                        if rule_sequence in var_seq_order.keys():
-                            var_seq_order[rule_sequence].append(1)
-                        else:
-                            var_seq_order[rule_sequence] = [1]
-
-                    if len(conf_seq_list) > 2:
-                        if rule_sequence in var_seq_order.keys():
-                            var_seq_order[rule_sequence].append(calculate_asc_consistency_ratio(conf_seq_list))
-                        else:
-                            var_seq_order[rule_sequence] = [calculate_asc_consistency_ratio(conf_seq_list)]
-
-                if r_param == "Mean Ascendingness Change":
-                    if len(conf_seq_list) == 1:
-                        if rule_sequence in var_seq_order.keys():
-                            var_seq_order[rule_sequence].append(conf_seq_list[0])
-                        else:
-                            var_seq_order[rule_sequence] = [conf_seq_list[0]]
-
-                    high = float(conf_sequence.split("->")[-1])
-                    low = float(conf_sequence.split("->")[0])
-
-                    if high - low >=0:
+                    high_lift = float(lift_sequence.split("->")[-1])
+                    low_lift = float(lift_sequence.split("->")[0])
+                    if (high - low)>=0 and (high_lift - low_lift) >= 0:
                         if len(conf_seq_list) == 2:
                             if rule_sequence in var_seq_order.keys():
-                                var_seq_order[rule_sequence].append(high - low)
+                                var_seq_order[rule_sequence].append(round((high - low),2))
                             else:
-                                var_seq_order[rule_sequence] = [high - low ]
+                                var_seq_order[rule_sequence] = [round((high-low),2)]
+                            if rule_sequence in var_seq_order_lift.keys():
+                                var_seq_order_lift[rule_sequence].append(round((high - low),2))
+                            else:
+                                var_seq_order_lift[rule_sequence] = [round((high-low),2)]
 
-                        if len(conf_seq_list) > 2:
-                            if rule_sequence in var_seq_order.keys():
-                                var_seq_order[rule_sequence].append(mean_asc_change(conf_seq_list))
-                            else:
-                                var_seq_order[rule_sequence] = [mean_asc_change(conf_seq_list)]
+                        # if len(conf_seq_list) > 2:
+                        #     if rule_sequence in var_seq_order.keys():
+                        #         var_seq_order[rule_sequence].append( high-low)
+                        #     else:
+                        #         var_seq_order[rule_sequence] = [high-low]
+
+
+                # if r_param == "Ascendingness Consistency Ratio":
+                #     if len(conf_seq_list) == 1:
+                #         if rule_sequence in var_seq_order.keys():
+                #             var_seq_order[rule_sequence].append(conf_seq_list[0])
+                #         else:
+                #             var_seq_order[rule_sequence] = [conf_seq_list[0]]
+                #     high = float(conf_sequence.split("->")[-1])
+                #     low = float(conf_sequence.split("->")[0])
+                #     # if (high - low >=-5000):
+                #     # if (high - low >=-5000):
+                #     if len(conf_seq_list) == 2:
+                #         if rule_sequence in var_seq_order.keys():
+                #             var_seq_order[rule_sequence].append(1)
+                #         else:
+                #             var_seq_order[rule_sequence] = [1]
+
+                #     if len(conf_seq_list) > 2:
+                #         if rule_sequence in var_seq_order.keys():
+                #             var_seq_order[rule_sequence].append(calculate_asc_consistency_ratio(conf_seq_list))
+                #         else:
+                #             var_seq_order[rule_sequence] = [calculate_asc_consistency_ratio(conf_seq_list)]
+
+                # if r_param == "Mean Ascendingness Change":
+                #     if len(conf_seq_list) == 1:
+                #         if rule_sequence in var_seq_order.keys():
+                #             var_seq_order[rule_sequence].append(conf_seq_list[0])
+                #         else:
+                #             var_seq_order[rule_sequence] = [conf_seq_list[0]]
+
+                #     high = float(conf_sequence.split("->")[-1])
+                #     low = float(conf_sequence.split("->")[0])
+
+                #     if high - low >=0:
+                #         if len(conf_seq_list) == 2:
+                #             if rule_sequence in var_seq_order.keys():
+                #                 var_seq_order[rule_sequence].append(high - low)
+                #             else:
+                #                 var_seq_order[rule_sequence] = [high - low ]
+
+                #         if len(conf_seq_list) > 2:
+                #             if rule_sequence in var_seq_order.keys():
+                #                 var_seq_order[rule_sequence].append(mean_asc_change(conf_seq_list))
+                #             else:
+                #                 var_seq_order[rule_sequence] = [mean_asc_change(conf_seq_list)]
     
     order_mean_dict = dict()
     order_mean_dict_lift = dict()
@@ -260,19 +263,21 @@ def createRulesTree(num_attr, df, r_param):
             if num_attr ==0:
                 for k in order_mean_dict.keys():
                     if len(k.split("->")) > 2:
-                        st.text(str(k) + "=> " + "Conf: "+ str(order_mean_dict[k]))
+                        st.text(str(k) + "=> " + "Conf Difference: "+ str(order_mean_dict[k]) + " Lift Difference: "+ str(order_mean_dict_lift[k]))
                         continue
                     for l in order_mean_dict_lift.keys():
                         if k == l:
-                            st.text(str(k) + "=> " + "Conf: "+ str(order_mean_dict[k] + " Lift: " + str(order_mean_dict_lift[k])))
+                            st.text(str(k) + "=> " + "Conf: "+ str(order_mean_dict[k]) + " Lift: " + str(order_mean_dict_lift[k]))
                             break
                 max_order = max(order_mean_dict, key=order_mean_dict.get)
                 st.text("Order with highest mean is: " + "\n \t" + max_order)
             else:
                 for k in order_mean_dict.keys():
-                    if len(k.split("->"))==num_attr:
-                        mean_dict_num_attr[k] = order_mean_dict[k]
-                        st.text(str(k) + "=> " + str(order_mean_dict[k]))
+                    for l in order_mean_dict_lift.keys():
+                        if len(k.split("->"))==num_attr and k ==l:
+                            mean_dict_num_attr[k] = order_mean_dict[k]
+                            st.text(str(k) + "=> " + "Conf: "+ str(order_mean_dict[k]) + " Lift: " + str(order_mean_dict_lift[k]))
+                            break
             if len(mean_dict_num_attr) == 0:
                 st.text("No rules of length " + str(num_attr) + " was found.")
             else:
@@ -384,20 +389,20 @@ def main(args):
     conf_threshold = [i for i in range(0, 101, 1)]
     confidence = st.sidebar.select_slider("Confidence Threshold", conf_threshold)
     confidence = confidence / 100
-    support = 0.01
-    confidence = 0
-    rule_parameter = "Mean Confidence"
+    # support = 0.01
+    # confidence = 0
+    # rule_parameter = "Mean Confidence"
     data_folder = "/Users/ashara/Documents/Study/Research/Dissertation/One Drive/OneDrive - University of Texas at Arlington/Dissertation/data_files/CSV"
     list_of_files = [f for f in os.listdir(data_folder)]
     filename = st.sidebar.selectbox("Select source data", sorted(list_of_files))
     data_file = data_folder + "/" + filename
     num_of_attr = [i for i in range(11)]
     attr_num = st.sidebar.selectbox("Number of Attributes", num_of_attr)
-    attr_num = 0
+    # attr_num = 0
     # data_file = data_folder + "/" + "Grouped.csv"
     # data_file = data_folder + "/" +  "test_FPGrowth.csv"
     # data_file = data_folder + "/" +  "Inpatient_Claims_PUF.csv"
-    data_file = data_folder + "/" +  "MIMIC_Sym_Proc_Diag_Drug.csv"
+    # data_file = data_folder + "/" +  "MIMIC_Sym_Proc_Diag_Drug.csv"
     st.text("Data File: " + data_file.split("/")[-1])
     st.text("Rule Parameter: " + rule_parameter)
     st.text("Support: " + str(support * 100) + "%")
